f43e9ef8938cff5a316557f597e6c14fcb5e832a
omecity <omeiza.olumoye@gmail.com>
2025-03-06 22:51:24 -0500

    added a test file to test the addtion method

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include "files.h"
+
+TEST(AdditionTest, HandlesPositiveNumbers) {
+    EXPECT_EQ(add(3, 4), 7);
+    EXPECT_EQ(add(10, 5), 15);
+}
+
+TEST(AdditionTest, HandlesNegativeNumbers) {
+    EXPECT_EQ(add(-3, -4), -7);
+    EXPECT_EQ(add(-10, 5), -5);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
